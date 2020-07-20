bb3260efba2f76a0517d451bcb11408507733e76
Pierre Contri <pierre.contri@free.fr>
2020-07-20 10:51:09 +0200

    Update the title part and take care about carriage returns

--- php/markdownHelper.php
+++ php/markdownHelper.php
@@ -8,31 +8,32 @@ class MarkdownAdapter {
 			// tables
 			'/((?:([^\r\n|]*)\|)+(?:([^\r\n|]*)))\r?\n(?:( ?:?-+:? ?)\|)+(?:( ?:?-+:? ?))\r?\n(((?:([^\r\n|]*)\|)+(?:([^\r\n|]*))\r?\n)+)/m',
 			'/(\|(?:([^\r\n|]*)\|)+)\r?\n\|(?:( ?:?-+:? ?)\|)+\r?\n((\|(?:([^\r\n|]*)\|)+\r?\n)+)/m',
-			// manage titles
-			'/^[#]{6}\s*(.+)$/m',
-			'/^[#]{5}\s*(.+)$/m',
-			'/^[#]{4}\s*(.+)$/m',
-			'/^[#]{3}\s*(.+)$/m',
-			'/^[#]{2}\s*(.+)$/m',
-			'/^[#]{1}\s*(.+)$/m',
-			// specific titles
-			'/^(.*)\n[=]+.*$/m',
-			'/^(.*)\n[-][-]+.*\n$/m',
+			// code
 			'/[\']{3}((.|\n)+?)[\']{3}/m',  // code bloc
-			// lines break
-			'/\n([- * _]{3,})\n/',
 			// manage lists
-			'/^[ \t]*(\w|\d)\.[ \t]+(.+)$\n\n/m',         // end of number
-			'/\n\n^[ \t]*([\w\d])\.[ \t]+(.+)$/m',        // start of number
+			'/^[ \t]*(\w|\d)\.[ \t]+(.+)$\n/m',         // end of number
+			'/^\n[ \t]*([\w\d])\.[ \t]+(.+)$/m',        // start of number
 			'/^[ \t]*[-\*][ \t]+(.+)\n$/m',               // end of list
-			'/\n\n^[ \t]*[-\*][ \t]+(.+)$/m',             // start of list
+			'/^\n[ \t]*[-\*][ \t]+(.+)$/m',             // start of list
 			'/^[ \t]*(([\d\w]\.)|([-\*]))[ \t]+(.+)$/m',  // content of list
+			// manage titles
+			'/^\r?\n?[#]{6}\s*(.+)\r?\n?$/m',
+			'/^\r?\n?[#]{5}\s*(.+)\r?\n?$/m',
+			'/^\r?\n?[#]{4}\s*(.+)\r?\n?$/m',
+			'/^\r?\n?[#]{3}\s*(.+)\r?\n?$/m',
+			'/^\r?\n?[#]{2}\s*(.+)\r?\n?$/m',
+			'/^\r?\n?[#]{1}\s*(.+)\r?\n?$/m',
+			// specific titles
+			'/^\r?\n?(.*)\r?\n[=]+.*\r?\n?$/m',
+			'/^\r?\n?(.*)\r?\n[-][-]+.*\r?\n?$/m',
+			// lines break
+			'/\n([-*_]{3,})\n/',
 			// manage caracters
 			'/([\W\s_])\*{3}(.*?)\*{3}([\W\s_])/',  // bolt && italic
 			'/([\W\s_])\*{2}(.*?)\*{2}([\W\s_])/',  // bolt
 			'/([\W\s_])\*{1}(.*?)\*{1}([\W\s_])/',  // italic
 			'/([\W\s])_(.*?)_([\W\s])/',            // underline
-			'/^\r?\n$/m',  // new line
+			'/(.*?\r?\n)(\r?\n)/m',  // new line
 			// paragraph
 			//'/<.*>((.|\n)*)<\/.*>/',
 			// links
@@ -47,31 +48,32 @@ class MarkdownAdapter {
 			// tables
 			"\n<table>\n  <tr><th>{{{{TH}}}}\${1}{{{{TH}}}}</th></tr>\n  <tr><td>{{{{TD}}}}\${6}{{{{TD}}}}</td></tr>\n</table>\n<br/>\n",
 			"\n<table>\n  <tr><th>{{{{TH}}}}\${1}{{{{TH}}}}</th></tr>\n  <tr><td>{{{{TD}}}}\${4}{{{{TD}}}}</td></tr>\n</table>\n<br/>\n",
-			// manage titles
-			"<h6>\${1}</h6>\n",
-			"<h5>\${1}</h5>\n",
-			"<h4>\${1}</h4>\n",
-			"<h3>\${1}</h3>\n",
-			"<h2>\${1}</h2>\n",
-			"<h1>\${1}</h1>\n",
-			// specific titles
-			"<h1>\${1}</h1>\n",
-			"<h2>\${1}</h2>\n",
+			// code
 			'<pre><code>${1}</code></pre>',  // code bloc
-			// lines break
-			'<hr />',
 			// manage lists
 			"<li class=\"endListNb\">\${2}</li></ol>\n",                                      // end of number
-			"\n\n<ol type=\"\${1}\" start=\"\${1}\"><li class=\"startListNb\">\${2}</li>\n",  // start of number
+			"<ol type=\"\${1}\" start=\"\${1}\"><li class=\"startListNb\">\${2}</li>",  // start of number
 			"<li class=\"endListLi\">\${1}</li></ul>\n",                                      // end of list
-			"\n\n<ul><li class=\"startListLi\">\${1}</li>\n",                                 // start of list
-			"<li class=\"contentList\">\${4}</li>\n",                                         // content of list
+			"<ul><li class=\"startListLi\">\${1}</li>",                                 // start of list
+			"<li class=\"contentList\">\${4}</li>",                                         // content of list
+			// manage titles
+			"<h6>\${1}</h6>",
+			"<h5>\${1}</h5>",
+			"<h4>\${1}</h4>",
+			"<h3>\${1}</h3>",
+			"<h2>\${1}</h2>",
+			"<h1>\${1}</h1>",
+			// specific titles
+			"<h1>\${1}</h1>",
+			"<h2>\${1}</h2>",
+			// lines break
+			'<hr />',
 			// manage caracters
 			'${1}<b><i>${2}</i></b>${3}',  // bolt && italic
 			'${1}<b>${2}</b>${3}',         // bolt
 			'${1}<i>${2}</i>${3}',         // italic
 			'${1}<u>${2}</u>${3}',         // underline
-			"<br />\n",  // new line
+			"\${1}<br />\n",  // new line
 			// paragraph
 			//'<p>${1}</p>',
 			// links
