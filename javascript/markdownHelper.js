2b39f927ebe058dcfbca54d4bde80ac3637757a4
Pierre Contri <pierre.contri@free.fr>
2020-07-20 11:01:04 +0200

    Update the title part and take care about carriage returns

--- javascript/markdownHelper.js
+++ javascript/markdownHelper.js
@@ -11,17 +11,24 @@ markdownHelper.rulesRegex = [
 	// tables
 	{ pattern : /((?:([^\r\n|]*)\|)+(?:([^\r\n|]*)))\r?\n(?:( ?:?-+:? ?)\|)+(?:( ?:?-+:? ?))\r?\n(((?:([^\r\n|]*)\|)+(?:([^\r\n|]*))\r?\n)+)/gm, replacement : "\n<table>\n  <tr><th>{{{{TH}}}}$1{{{{TH}}}}</th></tr>\n  <tr><td>{{{{TD}}}}$6{{{{TD}}}}</td></tr>\n</table>\n<br/>\n" },
 	{ pattern : /(\|(?:([^\r\n|]*)\|)+)\r?\n\|(?:( ?:?-+:? ?)\|)+\r?\n((\|(?:([^\r\n|]*)\|)+\r?\n)+)/gm, replacement : "\n<table>\n  <tr><th>{{{{TH}}}}$1{{{{TH}}}}</th></tr>\n  <tr><td>{{{{TD}}}}$4{{{{TD}}}}</td></tr>\n</table>\n<br/>\n" },
-	// manage titles
-	{ pattern : /^[#]{6}\s*(.+)$/gm, replacement : "<h6>$1</h6>\n" },
-	{ pattern : /^[#]{5}\s*(.+)$/gm, replacement : "<h5>$1</h5>\n" },
-	{ pattern : /^[#]{4}\s*(.+)$/gm, replacement : "<h4>$1</h4>\n" },
-	{ pattern : /^[#]{3}\s*(.+)$/gm, replacement : "<h3>$1</h3>\n" },
-	{ pattern : /^[#]{2}\s*(.+)$/gm, replacement : "<h2>$1</h2>\n" },
-	{ pattern : /^[#]{1}\s*(.+)$/gm, replacement : "<h1>$1</h1>\n" },
-	// specific titles
-	{ pattern : /^(.*)\n[=]+.*$/gm, replacement : "<h1>$1</h1>\n" },
-	{ pattern : /^(.*)\n[-][-]+.*\n$/gm, replacement : "<h2>$1</h2>\n" },
+	// code
 	{ pattern : /[\']{3}((.|\n)+)[\']{3}/gm, replacement : '<code>$1</code>' },  // code bloc
+	// manage lists
+	{ pattern : /^[ \t]*(\w|\d)\.[ \t]+(.+)$\n/gm,          replacement : "<li class=\"endListNb\">$2</li></ol>\n" },                                    // end of number
+	{ pattern : /\n\n^[ \t]*([\w\d])\.[ \t]+(.+)$/gm,       replacement : "<ol type=\"$1\" start=\"$1\"><li class=\"startListNb\">$2</li>" },            // start of number
+	{ pattern : /^[ \t]*[-\*][ \t]+(.+)\n$/gm,              replacement : "<li class=\"endListLi\">$1</li></ul>\n" },                                    // end of list
+	{ pattern : /\n\n^[ \t]*[-\*][ \t]+(.+)$/gm,            replacement : "<ul><li class=\"startListLi\">$1</li>" },                                     // start of list
+	{ pattern : /^[ \t]*(([\d\w]\.)|([-\*]))[ \t]+(.+)$/gm, replacement : "<li class=\"contentList\">$4</li>" },                                         // content of list
+	// manage titles
+	{ pattern : /^\r?\n?[#]{6}\s*(.+)\r?\n?$/gm, replacement : "<h6>$1</h6>" },
+	{ pattern : /^\r?\n?[#]{5}\s*(.+)\r?\n?$/gm, replacement : "<h5>$1</h5>" },
+	{ pattern : /^\r?\n?[#]{4}\s*(.+)\r?\n?$/gm, replacement : "<h4>$1</h4>" },
+	{ pattern : /^\r?\n?[#]{3}\s*(.+)\r?\n?$/gm, replacement : "<h3>$1</h3>" },
+	{ pattern : /^\r?\n?[#]{2}\s*(.+)\r?\n?$/gm, replacement : "<h2>$1</h2>" },
+	{ pattern : /^\r?\n?[#]{1}\s*(.+)\r?\n?$/gm, replacement : "<h1>$1</h1>" },
+	// specific titles
+	{ pattern : /^\r?\n?(.*)\n[=]+.*\r?\n?$/gm,    replacement : "<h1>$1</h1>" },
+	{ pattern : /^\r?\n?(.*)\n[-][-]+.*\r?\n?$/gm, replacement : "<h2>$1</h2>" },
 	// lines break
 	{ pattern : /\n([- * _]{3,})\n/g,  replacement : '<hr />' },
 	// manage caracters
@@ -29,12 +36,6 @@ markdownHelper.rulesRegex = [
 	{ pattern : /\s\*{2}(.+)\*{2}\s/g, replacement : '<b>$1</b>' },         // bolt
 	{ pattern : /\s\*{1}(.+)\*{1}\s/g, replacement : '<i>$1</i>' },         // italic
 	{ pattern : /\s_(.+)_\s/g,         replacement : '<u>$1</u>' },         // underline
-	// manage lists
-	{ pattern : /^[ \t]*(\w|\d)\.[ \t]+(.+)$\n\n/gm,        replacement : "<li class=\"endListNb\">$2</li></ol>\n\n" },                                    // end of number
-	{ pattern : /\n\n^[ \t]*([\w\d])\.[ \t]+(.+)$/gm,       replacement : "\n\n<ol type=\"$1\" start=\"$1\"><li class=\"startListNb\">$2</li>\n" },        // start of number
-	{ pattern : /^[ \t]*[-\*][ \t]+(.+)\n$/gm,              replacement : "<li class=\"endListLi\">$1</li></ul>\n\n" },                                    // end of list
-	{ pattern : /\n\n^[ \t]*[-\*][ \t]+(.+)$/gm,            replacement : "\n\n<ul><li class=\"startListLi\">$1</li>\n" },                                 // start of list
-	{ pattern : /^[ \t]*(([\d\w]\.)|([-\*]))[ \t]+(.+)$/gm, replacement : "<li class=\"contentList\">$4</li>\n" },                                         // content of list
 
 	{ pattern : /^\r?\n$/gm,                                   replacement : "<br />\n"},  // new line
 	// paragraph
