78855da58b5348e6ce61a7269313103eb552fe77
Pierre Contri <pierre.contri@free.fr>
2020-09-20 15:17:35 +0200

    Update php part

--- php/index.php
+++ php/index.php
@@ -4,8 +4,23 @@ include "markdownHelper.php";
 
 // index.php?read=tests.md
 $fileToRead = $_GET["read"];
-$htlmPage = ($fileToRead != "") ? MarkdownAdapter::transformMdToHtml(file_get_contents($fileToRead)) : MarkdownAdapter::test_md() ;
+$htmlPage = ($fileToRead != "") ? MarkdownAdapter::transformMdToHtml(file_get_contents($fileToRead)) : MarkdownAdapter::test_md() ;
 
-print($htlmPage);
+// print($htmlPage);
+
+$fullPage = <<<EndText
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Transform Markdown to HTML helper</title>
+    <meta charset=UTF-8 />
+  </head>
+  <body>
+    <div id="mdText">${htmlPage}</div>
+  </body>
+</html>
+EndText;
+
+print($fullPage);
 
 ?>
